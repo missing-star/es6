3a17c9a348410e049fd9c9f502ea870b9c70a5be
DESKTOP-5K7FJ6E\thunder <1652575963@qq.com>
2018-09-25 18:16:20 +0800

    async

---
+++ readFile.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+
+/**
+ * Promise方式
+ * @param fileName
+ * @returns {Promise<any>}
+ */
+function readFile(fileName) {
+    return new Promise((resolve,reject) => {
+        fs.readFile(fileName,(err,data) => {
+            if(err) reject(err);
+            resolve(data);
+        });
+    });
+}
+
+// readFile('data/a.txt').then(res => {
+//     console.log(res.toString());
+//     return readFile('data/b.txt');
+// }).then(res => {
+//     console.log(res.toString());
+//     return readFile('data/c.txt');
+// }).then(res => {
+//     console.log(res.toString());
+// });
+
+/**
+ * generator方式
+ */
+function * gen() {
+    yield readFile('data/a.txt');
+    yield readFile('data/b.txt');
+    yield readFile('data/c.txt');
+}
+
+let g = gen();
+// g.next().value.then(res => {
+//     console.log(res.toString());
+//     return g.next().value;
+// }).then(res => {
+//     console.log(res.toString());
+//     return g.next().value;
+// }).then(res => {
+//     console.log(res.toString());
+// });
+
+/**
+ * async方式
+ * @returns {Promise<void>}
+ */
+async function fn() {
+    let f1 = await readFile('data/a.txt');
+    console.log(f1.toString());
+}
+
+fn();
