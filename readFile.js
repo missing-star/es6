211077ffcdc133154320d6708d7e5b2c3176fbe3
DESKTOP-5K7FJ6E\thunder <1652575963@qq.com>
2018-10-25 15:26:37 +0800

    async

--- readFile.js
+++ readFile.js
@@ -51,6 +51,8 @@ let g = gen();
 async function fn() {
     let f1 = await readFile('data/a.txt');
     console.log(f1.toString());
+    let f2 = await readFile('data/b.txt');
+    console.log(f2.toString());
 }
 
 fn();
